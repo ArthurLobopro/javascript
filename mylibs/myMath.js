a5915ae2283dde9a645cb18e008d3bc56c944b2d
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-01 02:18:17 -0300

    Adição da função de raiz.

--- mylibs/myMath.js
+++ mylibs/myMath.js
@@ -1,5 +1,8 @@
 const myMath = {
-    pow: function(num,expo=2) {
+    pow: function(num,expo=2){
         return num**expo
+    },
+    sqrt: function(num,expo=2){
+        return num**1/expo
     }
 }
