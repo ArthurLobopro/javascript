4a698247ff90a6b180fd78204c345579866feabe
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-13 21:39:12 -0300

    Update correct_number.js

--- mylibs/correct_number.js
+++ mylibs/correct_number.js
@@ -1,7 +1,28 @@
 function soma_dec(n1,n2){
     let multi = 1
-    n1_l = String(n1).length - String(n1).indexOf('.') - 1
-    n2_l = String(n2).length - String(n2).indexOf('.') - 1
+    let n1_l = String(n1).length - String(n1).indexOf('.') - 1
+    let n2_l = String(n2).length - String(n2).indexOf('.') - 1
     multi*= (n1_l>n2_l)? 10**n1_l : 10**n2_l
     return (n1 * multi + n2 * multi)/multi
+}
+function sub_dec(n1,n2){
+    let multi = 1
+    let n1_l = String(n1).length - String(n1).indexOf('.') - 1
+    let n2_l = String(n2).length - String(n2).indexOf('.') - 1
+    multi*= (n1_l>n2_l)? 10**n1_l : 10**n2_l
+    return (n1 * multi - n2 * multi)/multi
+}
+function mult_dec(n1,n2){
+    let multi = 1
+    let n1_l = String(n1).length - String(n1).indexOf('.') - 1
+    let n2_l = String(n2).length - String(n2).indexOf('.') - 1
+    multi*= (n1_l>n2_l)? 10**n1_l : 10**n2_l
+    return (n1*multi) * (n2*multi) / (multi*multi)
+}
+function div_dec(n1,n2){
+    let multi = 1
+    let n1_l = String(n1).length - String(n1).indexOf('.') - 1
+    let n2_l = String(n2).length - String(n2).indexOf('.') - 1
+    multi*= (n1_l>n2_l)? 10**n1_l : 10**n2_l
+    return (n1*multi) / (n2*multi)
 }
