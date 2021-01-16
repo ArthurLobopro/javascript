cfeea68a495c221aede6ea719207edab5664a20b
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-16 20:37:11 -0300

    estruturas de cookie

--- mygames/jo-ken-po/index.js
+++ mygames/jo-ken-po/index.js
@@ -1,8 +1,34 @@
+let player = 0
+let bot = 0
 const opcoes = {
     caminho: [ 'midia/pedra.svg','midia/papel.svg','midia/tesoura.svg'],
     texto: ['JO','KEN','PO']
 }
 const content = document.getElementById('content')
+function lercookie(){
+    let date = document.cookie.indexOf('placar')
+    if(date==-1){
+        criaCookie()
+    }else{
+        const cook = document.cookie
+        let x = cook.indexOf('x',date)
+        player=Number(cook.substring(7,x))
+        bot = Number(cook.substring(x+1,cook.length))
+        console.log(player)
+        console.log(bot)
+    }
+    placar()
+}
+function criaCookie(player=0,bot=0){
+    let date = new Date()
+    date.setMonth(date.getMonth()+1)
+    document.cookie=`placar=${player}x${bot};expires=${date};path=/;`
+    lercookie()
+}
+function placar(){
+    document.getElementById('player').innerText=player
+    document.getElementById('bot').innerText=bot
+}
 function randint(min,max){
     return Math.floor(Math.random() * (max-min+1)) + min
 }
@@ -46,9 +72,13 @@ function resultado(jogador,computador){
     let msg = 'Você '
     const win = () => {
         msg += 'GANHOU!!!'
+        player++
+        criaCookie(player,bot)
     }
     const lose = () =>{
         msg += 'PERDEU!!!'
+        bot++
+        criaCookie(player,bot)
     }
     const empate = () =>{
         msg = 'EMPATE!!!'
@@ -75,6 +105,21 @@ function resultado(jogador,computador){
             <div class='pc'><img src="midia/${computador}.svg"> Bot</div>
             <div class='result'><span class="msg">${msg}</span></div>
         </div>
-        
+    </div>`
+    placar()
+}
+function restart(){
+    content.innerHTML=`
+    <div id="escolha">Escolha sua jogada:</div>
+    <div id="options">
+        <div class="circle" onclick="jogada('pedra')">
+            <img src="midia/pedra.svg" alt="Pedra" title="Pedra">
+        </div>
+        <div class="circle" onclick="jogada('papel')">
+            <img src="midia/papel.svg" alt="Papel" title="Papel">
+        </div>
+        <div class="circle" onclick="jogada('tesoura')">
+            <img src="midia/tesoura.svg" alt="Tesoura" title="Tesoura">
+        </div>
     </div>`
 }
