b4c6f4ab67a6a58f2031b8bf2187f11c2ad35f86
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-02-20 20:50:48 -0300

    Função binário para decimal

--- mylibs/bases.js
+++ mylibs/bases.js
@@ -8,4 +8,13 @@ const dec_to_bin = dec => {
     }while (!found)
     return String(bin.reverse().join(''))
 }
-export {dec_to_bin}
+let bin_to_dec = bin => {
+    bin = String(bin).split('').reverse()
+    let mult = 1, dec = 0
+    for(let i of bin){
+        dec+=mult*Number(i)
+        mult*=2
+    }
+    return dec
+}
+export {dec_to_bin, bin_to_dec}
