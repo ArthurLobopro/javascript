319864537bd218528725c19794e796aef8c996bd
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-02-21 01:46:45 -0300

    otimização de código

--- mylibs/bases.js
+++ mylibs/bases.js
@@ -8,17 +8,19 @@ const dec_to =  (dec,base) => {
     }while (!found)
     return String(num.reverse().join(''))
 }
-
-const dec_to_bin = dec => dec_to(dec,2)
-const dec_to_oct = dec => dec_to(dec,8)
-
-const bin_to_dec = bin => {
-    bin = String(bin).split('').reverse()
+const to_dec = (num,base) => {
+    num = String(num).split('').reverse()
     let mult = 1, dec = 0
-    for(let i of bin){
+    for(let i of num){
         dec+=mult*Number(i)
-        mult*=2
+        mult*=base
     }
     return dec
 }
-export {dec_to_bin, dec_to_oct, bin_to_dec}
+
+const dec_to_bin = dec => dec_to(dec,2)
+const dec_to_oct = dec => dec_to(dec,8)
+const bin_to_dec = bin => to_dec(bin,2)
+const oct_to_dec = oct => to_dec(oct,8)
+
+export {dec_to, dec_to_bin, dec_to_oct, to_dec, bin_to_dec, oct_to_dec}
