0acb04452e1083e6d07af249389e528c9c1816bc
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-02-21 02:05:47 -0300

    função decimal para binário

--- mylibs/bases.js
+++ mylibs/bases.js
@@ -6,7 +6,7 @@ const dec_to =  (dec,base) => {
         dec = Math.floor(dec/base)
         found = dec==0 ? true : false
     }while (!found)
-    return String(num.reverse().join(''))
+    return base == 16 ? num : String(num.reverse().join(''))
 }
 const to_dec = (num,base) => {
     num = String(num).split('').reverse()
@@ -23,4 +23,11 @@ const dec_to_oct = dec => dec_to(dec,8)
 const bin_to_dec = bin => to_dec(bin,2)
 const oct_to_dec = oct => to_dec(oct,8)
 
-export {dec_to, dec_to_bin, dec_to_oct, to_dec, bin_to_dec, oct_to_dec}
+const dec_to_hex = dec => {
+    const hexn = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+    let hex = dec_to(dec,16)
+    for(let i in hex){ hex[i] = hexn[ hex[i] ] }
+    return String(hex.reverse().join(''))
+}
+
+export {dec_to, dec_to_bin, dec_to_oct, dec_to_hex, to_dec, bin_to_dec, oct_to_dec}
