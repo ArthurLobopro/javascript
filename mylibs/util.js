0a8adabad0fa2215ddf4d59142c870ebaec9e146
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-02-22 14:51:17 -0300

    Update util.js

--- mylibs/util.js
+++ mylibs/util.js
@@ -1,10 +1,12 @@
-const util = {
-    range: (min,max,pass=1) => {
-        let array = []
-        for(i = min;i<max;i+=pass){ array.push(i) }
-        return array
-    },
-
+const range = (min,max,pass=1) => {
+    let array = []
+    for(i = min;i<max;i+=pass){ array.push(i) }
+    return array
 }
-export const range = util.range
-export default util
+const reverse = str =>{
+    let array = String(str).split('')
+    array.reverse()
+    array = String(array).replaceAll(',','')
+    return array
+}
+export { range, reverse}
