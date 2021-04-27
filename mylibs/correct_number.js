201c5f335a97a21d59ff312cb24de5f516e959ef
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-04-26 23:37:32 -0300

    função pow adicionada

--- mylibs/correct_number.js
+++ mylibs/correct_number.js
@@ -1,29 +1,44 @@
-function soma_dec(n1,n2){
+function soma(n1,n2){
     let multi = 1
     let n1_l = String(n1).length - String(n1).indexOf('.') - 1
     let n2_l = String(n2).length - String(n2).indexOf('.') - 1
     multi*= (n1_l>n2_l)? 10**n1_l : 10**n2_l
     return (n1 * multi + n2 * multi)/multi
 }
-function sub_dec(n1,n2){
+
+function sub(n1,n2){
     let multi = 1
     let n1_l = String(n1).length - String(n1).indexOf('.') - 1
     let n2_l = String(n2).length - String(n2).indexOf('.') - 1
     multi*= (n1_l>n2_l)? 10**n1_l : 10**n2_l
     return (n1 * multi - n2 * multi)/multi
 }
-function mult_dec(n1,n2){
+
+function mult(n1,n2){
     let multi = 1
     let n1_l = String(n1).length - String(n1).indexOf('.') - 1
     let n2_l = String(n2).length - String(n2).indexOf('.') - 1
     multi*= (n1_l>n2_l)? 10**n1_l : 10**n2_l
     return (n1*multi) * (n2*multi) / (multi*multi)
 }
-function div_dec(n1,n2){
+
+function div(n1,n2){
     let multi = 1
     let n1_l = String(n1).length - String(n1).indexOf('.') - 1
     let n2_l = String(n2).length - String(n2).indexOf('.') - 1
     multi*= (n1_l>n2_l)? 10**n1_l : 10**n2_l
     return (n1*multi) / (n2*multi)
 }
-export {soma_dec, sub_dec, mult_dec, div_dec}
+
+function pow(n, exp = 2) {
+    let mult = 1
+    let n_l = String(n).length - String(n).indexOf('.') - 1
+    mult = 10 ** n_l
+    let total = 1
+    for(let i =0;i<exp; i++){
+        total*= (n * mult)
+    }
+    return total / mult ** exp
+}
+
+export { soma, sub, mult, div, pow }
